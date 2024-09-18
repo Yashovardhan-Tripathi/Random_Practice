c2abe177eb24a5a223c64142d668e38b3cc5e1e3
Yashovardhan-Tripathi <yashovardhan0001@gmail.com>
2024-09-18 21:48:13 +0530

    random
    xyz

---
+++ first.cpp
@@ -0,0 +1,18 @@
+// sum of two numbers    --->Single Line Comment
+/*this
+is
+multi
+line
+comment
+*/
+#include <iostream>
+using namespace std;
+int w = 10;//global variable
+
+int main()
+{
+    int x = 10;//local variable hg
+    cout << "Value of x is : " << x << endl; // This will print local variable
+    cout << "Value of w is : " << w;         // this will print globle variable
+    return 0;
+}
